189f11af4e80f858e063c4c1c99fd309a3aa8bc5
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-11-13 10:11:22 +0100

    add view code in homeView.php

--- view/homeView.php
+++ view/homeView.php
@@ -2,6 +2,78 @@
 
 
 
+<?php if (isset($message)) {
+  echo $message;
+} ?>
+<main class="mt-5 container card">
+  <form class="mb-5" action="index.php" method="post">
+    <h3>Add account:</h3>
+    <input type="text" name="name" placeholder="Name">
+    <input type="submit" name="addAccount" value="Ok">
+  </form>
+
+  <hr>
+  <hr>
+
+  <div class="mt-4 accounts">
+    <div class="d-flex">
+      <h5 class="w-25"><strong>ID</strong></h5>
+      <h5 class="w-25"><strong>Name</strong></h5>
+      <h5 class="w-25"><strong>Solde</strong></h5>
+      <h5 class="w-25"><strong>Options</strong></h5>
+    </div>
+
+    <?php
+      foreach ($accounts as $account) {
+        ?>
+        <div class="account">
+          <div class="d-flex infos">
+            <h5 class="w-25"><?php echo $account->id() ?></h5>
+            <h5 class="w-25"><?php echo $account->name() ?></h5>
+            <h5 class="w-25"><?php echo $account->solde() ?></h5>
+            <h5 class="w-25"><button type="button" name="button">USE</button></h5>
+          </div>
+          <form class="options" action="index.php" method="post">
+            <input type="hidden" name="solde" value="<?php echo $account->solde()?>">
+            <input type="hidden" name="id" value="<?php echo $account->id()?>">
+            <div class="row">
+              <div class="option col-md-4 col-sm-12">
+                <h4 class="">Add money</h4>
+                <input class="w-75" type="text" name="amountAdd" placeholder="€">
+                <input type="submit" name="addMoney" value="Ok">
+              </div>
+
+              <div class="option col-md-4 col-sm-12">
+                <h4 class="">Output money</h4>
+                <input class="w-75" type="text" name="amountOutput" placeholder="€">
+                <input type="submit" name="outputMoney" value="Ok">
+              </div>
+
+              <div class="option col-md-4 col-sm-12">
+                <h4 class="">Transfert money</h4>
+                <input class="w-75" type="text" name="amountTransfert" placeholder="€"><br>
+                <label for="">Transfert to ID:</label>
+                <select class="" name="idAddMoneyAccount">
+                  <option value="">--</option>
+                  <?php for ($i=0; $i < count($listIdAccounts); $i++) {
+                    ?>
+                    <option value="<?php echo $listIdAccounts[$i] ?>"><?php echo $listIdAccounts[$i] ?></option>
+                    <?php
+                  } ?>
+                </select>
+                <input type="submit" name="transfertMoney" value="Ok">
+              </div>
+              <br>
+              <button class="btn btn-danger" type="submit" name="suppAccount">Delete account</button>
+
+            </div>
+          </form>
+        </div>
+        <?php
+      }
+      ?>
+  </div>
+</main>
 
 
 
